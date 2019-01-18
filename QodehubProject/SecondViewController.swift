16ac8c03a0709c09bd269330e32459adbc20e88b
ernest <ernestyawgaisie@gmail.com>
2019-01-18 13:29:49 +0000

    Initial Commit

---
+++ QodehubProject/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QodehubProject
+//
+//  Created by Ernest Gaisie on 18/01/2019.
+//  Copyright © 2019 Ernest Gaisie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
